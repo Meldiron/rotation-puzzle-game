f1636b66a8869b81df13bf306dcc683aee65b55c
Matej Bačo <matejbaco2000@gmail.com>
2024-09-20 14:11:48 +0000

    Reimplement for myself

--- game.js
+++ game.js
@@ -6,28 +6,16 @@ const getRandomElementFromArray = array => {
 const voiceFiles = {
   flip: [
     'flip'
-  ],
-  applause: [
-    'wow1', 'wow2', 'wow3', 'wow4', 'wow5',
-    'woo-hoo1', 'woo-hoo2', 'woo-hoo3', 'woo-hoo4',
   ]
 }
 
-const playSound = (type = 'applause') => {
+const playSound = (type = 'flip') => {
   const voiceFile = getRandomElementFromArray(voiceFiles[type])
   const audio = new Audio(`./assets/sound/${voiceFile}.mp3`)
   audio.play()
 }
 
-const playMusic = () => {
-  const musicFile = './assets/music/music.mp3'
-  const audio = new Audio(musicFile)
-  audio.volume = 0.35
-  audio.loop = true
-  audio.play()
-}
-
-const handleCardClick = event => {
+const handleCardClick = (event, isRightClick) => {
   event.preventDefault()
 
   const cardClicked = event.composedPath().find(element => (
@@ -35,54 +23,67 @@ const handleCardClick = event => {
   )
 
   if (!cardClicked.disabled) {
-    cardClicked.disabled = true
+
     playSound('flip')
 
+    const direction = isRightClick ? -1 : 1;
+
     const currentRotation = cardClicked.dataset.rotation
-    const newRotation = `${Number(currentRotation) + 1}`
+    let newRotation = `${Number(currentRotation) + direction}`
+
+    if(newRotation === "4") {
+      newRotation = "0"
+    }
+
+    if(newRotation === "-1") {
+      newRotation = "3"
+    }
+
+    let t = document.querySelector('#' + cardClicked.id + " div").style.transform;
+    if(!t) {
+      t = "rotate(0deg)";
+    }
+
+    let n = +(t.split("(")[1].split("deg")[0]);
+    n += isRightClick ? -90 : 90;
+    document.querySelector('#' + cardClicked.id + " div").style.transform = `rotate(${n}deg)`;
 
     cardClicked.dataset.rotation = newRotation
-
-    if (newRotation === "4") {
-      const imageFragment = cardClicked.querySelector('.imageFragment')
-      cardClicked.addEventListener('transitionend', () => {
-        imageFragment.style.transitionDuration = "0s"
-        requestAnimationFrame(() => {
-          cardClicked.dataset.rotation = "0"
-          requestAnimationFrame(() => {
-            imageFragment.style.transitionDuration = ""
-          })
-        })
-      }, {once: true})
-    }
 
     const allCards = [...document.querySelectorAll('.card')]
     const isPuzzleComplete = allCards.every(card => ["0", "4"].includes(card.dataset.rotation))
     if (isPuzzleComplete) {
-      allCards.forEach(card => card.disabled = true)
-      setTimeout(playSound, 1000)
-      setTimeout(() => {
-        currentPuzzleIndex += 1
-        if (currentPuzzleIndex === puzzles.length) {
-          currentPuzzleIndex = 0
-        }
-        setupBoard(puzzles[currentPuzzleIndex], 3)
-      }, 3000)
-    }
-    else {
-      cardClicked.addEventListener('transitionend', () => {
-        cardClicked.disabled = false
-      }, {once: true})
+      allCards.forEach(card => {
+        card.disabled = true
+        card.style.margin = '0px';
+      })
+      document.getElementById('board').classList.add("finished");
+      winAnimation()
     }
   }
 }
 
 const setupBoard = (puzzle, size = 4) => {
   const getRandomRotation = () => Math.floor(Math.random() * 4)
+  const result = getRandomRotation()
+
+  let rotation = 0;
+
+  if(result == 0) {
+    rotation = 0;
+  } else  if(result == 1) {
+    rotation = 90;
+  } else  if(result == 2) {
+    rotation = 180;
+  } else  if(result == 3) {
+    rotation = 270;
+  }
+
   const renderCard = (x, y) => `
     <button
+      id="if_${x}_${y}"
       class="card"
-      data-rotation="${getRandomRotation()}"
+      data-rotation="${result}"
       style="
         width: ${(80 / size)}vmin;
         height: ${(80 / size)}vmin;
@@ -91,13 +92,16 @@ const setupBoard = (puzzle, size = 4) => {
       <div
         class="imageFragment"
         style="
-          background-image: url('./assets/puzzles/${puzzle}.webp');
+          transform: rotate(${rotation}deg);
+          background-image: url('https://cloud.appwrite.io/v1/storage/buckets/images/files/${puzzle}/preview?project=rotation-puzzle-game&project=rotation-puzzle-game&format=webp&width=1720&height=1720&gravity=center');
           background-position: ${y * (100 / (size - 1))}% ${x * (100 / (size - 1))}%;
           background-size: ${(size) * 100}% ${(size) * 100}%;
         "
       ></div>
     </button>
   `
+
+
   const renderRow = children => `
     <div class="row">${children}</div>
   `
@@ -111,27 +115,27 @@ const setupBoard = (puzzle, size = 4) => {
   )).join('')
   document.getElementById('board').innerHTML = boardHTML
   document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', handleCardClick)
-    card.addEventListener('contextmenu', handleCardClick)
-    card.addEventListener('auxclick', handleCardClick)
+    card.addEventListener('click', (event) => handleCardClick(event, false))
+    card.addEventListener('contextmenu', (event) => handleCardClick(event, true))
   })
 }
 
-const puzzles = [
-  'balloons',
-  'cake-slice',
-  'candles',
-  'blue-balloons',
-  'cake',
-  'flag',
-  'party-hats',
-  'cupcakes',
-  'presents',
-  'toy'
-]
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
 
-let currentPuzzleIndex = Math.floor(Math.random() * puzzles.length)
+/**
+ * TEST
+ */
+const test = "3;66ed80b700341e1eef65";
+console.log(btoa(test));
 
-setupBoard(puzzles[currentPuzzleIndex], 3)
+var gameData = atob(urlParams.get('game')).split(";")
+console.log(gameData);
 
-document.addEventListener('click', playMusic, {once: true})
+setupBoard(gameData[1], +gameData[0])
+
+const winAnimation = () => {
+  const confettiSettings = { "target": "confetti", "max": "80", "size": "1", "animate": true, "props": ["circle", "square", "triangle"], "colors": [[165, 104, 246], [230, 61, 135], [0, 199, 228], [253, 214, 126]], "clock": "15", "rotate": false, "start_from_edge": false, "respawn": true }
+  var confetti = new ConfettiGenerator(confettiSettings);
+  confetti.render();
+}
